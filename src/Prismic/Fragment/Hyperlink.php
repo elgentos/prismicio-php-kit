8609dfc57ae6160b3c964cc51fc1d4da8e1a1779
Peter Jaap Blaakmeer <peterjaap@blaakmeer.com>
2025-11-10 22:17:29 +0100

    Refactor render method parameters for clarity

--- src/Prismic/Fragment/Hyperlink.php
+++ src/Prismic/Fragment/Hyperlink.php
@@ -8,7 +8,11 @@ class Hyperlink implements BlockInterface
 {
     use MagicTrait, AttributesTrait, HtmlSerializerTrait;
 
-    public function render(string $content, LinkResolver $linkResolver = null, \closure $htmlSerializer = null): string
+    public function render(
+        string $content, 
+        ?LinkResolver $linkResolver = null, 
+        ?\closure $htmlSerializer = null
+    ): string
     {
         if ($result = $this->renderHtmlSerializer($content, $htmlSerializer)) {
             return $result;
